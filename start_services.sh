197d28cb26fcefb7a7266ecdb892b9bd1c14e359
Ivan Novikov <ivannovikov1303@gmail.com>
2025-11-19 21:21:01 +0300

    start services sh fix

--- start_services.sh
+++ start_services.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+STORAGE_DIR="$(pwd)/qdrant_storage"
+
+if [ ! -d "$STORAGE_DIR" ]; then
+    echo "Storage directory not found. Creating..."
+    mkdir -p "$STORAGE_DIR"
+else
+    echo "Found existing storage at $STORAGE_DIR"
+    chmod -R 777 "$STORAGE_DIR"
+fi
+
 
 if [ $(docker ps -a -f name=qdrant-dev | grep -w qdrant-dev | wc -l) -eq 1 ]; then
     echo "'qdrant-dev' exists. Restarting..."
